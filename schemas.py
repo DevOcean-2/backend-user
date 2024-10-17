fc35c63b8a3ae82de7a7f739049f72e401d6e9e1
namkidong98 <rlehd201@naver.com>
2024-10-17 17:50:58 +0900

    Feat: Add Admin Router with Vaccination and Allergy APIs

--- schemas.py
+++ schemas.py
@@ -102,3 +102,13 @@ class DogBreed(BaseModel):
     id : int
     name: str
     # description: Optional[str] = None
+
+class Vaccination(BaseModel):
+    id : int
+    name: str
+    # description: Optional[str] = None
+
+class Allergy(BaseModel):
+    id : int
+    name: str
+    # description: Optional[str] = None
